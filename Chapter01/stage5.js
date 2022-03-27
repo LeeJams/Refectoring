4935f31a7d498b700031a9751a87df645bfcbedf
LeeJams <89899636+LeeJams@users.noreply.github.com>
2022-03-28 00:19:31 +0900

    반복문 쪼개기, 문장 슬라이드하기

---
+++ Chapter01/stage5.js
@@ -0,0 +1,89 @@
+import json from "./JsonData.js";
+
+/* 
+반복문 쪼개기, 문장 슬라이드하기/ 임시 변수를 질의 함수로 바꾸기
+*/
+
+function statement(invoice, plays) {
+  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+  
+  for (let perf of invoice.performances) {
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
+      perf.audience
+    }석)\n`;
+  }
+
+  result += `총액: ${usd(totalAmount())}\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+  return result;
+
+  function amountFor(aPerformance) {
+    let result = 0; 
+
+    switch (playFor(aPerformance).type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 1000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+
+      default:
+        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+    }
+
+    return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  function volumeCreditsFor(perf) {
+    let result = 0;
+    result += Math.max(perf.audience - 30, 0);
+    if ("comedy" === playFor(perf).type) result += Math.floor(perf.audience / 5);
+    return result;
+  }
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    }).format(aNumber/100);
+  }
+
+  function totalVolumeCredits() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += volumeCreditsFor(perf);
+    }
+    return result;
+  }
+
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result
+  }
+}
+
+console.log(statement(json.invoice, json.plays));
+/* 
+청구 내역 (고객명: BigCo)
+ Hamlet: $650.00 (55석)
+ As You Like It: $490.00 (35석)
+ Othello: $500.00 (40석)
+총액: $1,640.00
+적립 포인트: 47점
+*/
